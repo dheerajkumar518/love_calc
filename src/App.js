7c6dd5dd933a9ae19d199a4094b076e59873719e
Dheeraj <sahdheeraj518@gmail.com>
2022-04-24 15:32:02 +0530

    Updated app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+const [Data,setData]=useState(null);
+const [Data1,setData1]=useState(null);
+const [results,setresults]=useState(null);
+const resultcal=()=>{
+   if((Data==="Dheeraj" && Data1==="Saritha")||(Data==="Saritha" && Data1==="Dheeraj")||(Data==="dheeraj" && Data1==="saritha")||(Data==="saritha" && Data1==="dheeraj")){
+     return setresults(100);
+   }
+else
+   {
+     var n=Math.random();
+   n=n*100;
+   n=Math.floor(n)+1;
+   return setresults(n);}
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+<>
+<div className="container">
+  <p>The chance of a relationship working out between "{Data}" and "{Data1}"  is {results}%</p>
+<h1>Love Calculator Result</h1>
+    <div className="nameShow">
+      <h1 hidden>A</h1>
+          <h3>{Data}</h3>
+          <div style={{fontSize:results}}>❤️</div>
+          <h2>{Data1}</h2>
+      <h1 hidden>B</h1>
     </div>
+
+    <div className="result">
+      <span>{results}%</span>
+    </div>
+
+    <div className="inputbox">
+      <input type="text" name="" id="" onChange={(val)=>setData(val.target.value)} /><span color="black">And</span>
+      <input type="text" onChange={(val)=>setData1(val.target.value)} /> <br />
+      <button onClick={()=>resultcal()}>Check</button>
+    </div>
+
+</div>
+<footer>
+      <code>
+        created by:Dheeraj Kumar
+      </code>
+    </footer>
+</>
   );
 }
 
